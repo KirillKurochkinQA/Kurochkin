2ca490cfe901d3d8884abb8afad7893a218f6c7e
k.kurochkin <kirill.kurochkin@synchro.pro>
2025-12-27 14:18:18 +0300

    upd Lesson Selenium 27/12

--- LessonSelenium1.py
+++ LessonSelenium1.py
@@ -3,4 +3,23 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 driver.get("https://www.saucedemo.com")
-time.sleep(5)
+page_url = driver.current_url
+print(page_url)
+assert page_url == "https://www.saucedemo.com/", "Адрес URL изменен"
+page_title = driver.title
+print(page_title)
+assert page_title == "Swag Labs", "Title изменен"
+driver.quit()
+
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+page_title = driver.title
+assert page_title == "Swag Labs", "Title изменен"
+print("Тест на проверку Title прошел успешно")
+driver.quit()
+
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+assert len(driver.find_elements("xpath", "//input[@data-test='username']")) == 1, f"Неверное количество полей ввода логина: {len(driver.find_elements("xpath", "//input[@data-test='username']"))}"
+print("Тест на проверку поля ввода логина прошел успешно")
+driver.quit()
