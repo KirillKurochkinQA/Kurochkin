bba42a3802558a06b3e8f87df59d6a90ef00bc10
k.kurochkin <kirill.kurochkin@synchro.pro>
2026-02-14 12:55:08 +0300

    upd after Lection Pytest 1

---
+++ test_pytest1.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+
+class TestLogin: # Название тестового класса
+
+    def test_open_login_page(self): # Название теста
+        driver = webdriver.Chrome()
+        driver.get("https://demoqa.com/login")
+        assert driver.current_url == "https://demoqa.com/login", "Открыта некорректная страница"
