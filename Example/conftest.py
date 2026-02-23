1cac25d77e2069c0f5edeebbf0b7c69f5da92423
k.kurochkin <kirill.kurochkin@synchro.pro>
2026-02-23 10:44:06 +0300

    upd 23/02/2026

--- Example/conftest.py
+++ Example/conftest.py
@@ -1,17 +1,25 @@
 import pytest
 from collections import namedtuple
+from selenium import webdriver
+import time
 from faker import Faker
 fake = Faker()
 
-@pytest.fixture()
-def generate_data():
-    login = fake.email()
-    password = fake.password()
-    NewUser = namedtuple('UserData', ['login', 'password'])  # именованный tuple
-    return NewUser(login, password) # Возвращаем обьект
+# @pytest.fixture()
+# def generate_data():
+#    login = fake.email()
+#    password = fake.password()
+#    NewUser = namedtuple('UserData', ['login', 'password'])  # именованный tuple
+#    return NewUser(login, password) # Возвращаем обьект
 
-@pytest.fixture(name="driver")
-def generate_data1(request):
-    # Генерируем данные
-    request.cls.login = fake.email()
-    request.cls.password = fake.password()
+#@pytest.fixture(name="driver")
+#def generate_data1(request):
+#    # Генерируем данные
+#    request.cls.login = fake.email()
+#    request.cls.password = fake.password()
+
+@pytest.fixture(autouse=True)
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
---
+++ Example/test_pytest4.py
@@ -0,0 +1,6 @@
+import pytest
+
+class TestExample:
+
+    def test_example(self, driver):
+        driver.get("https://google.com")
