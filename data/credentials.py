eb11c49ac30f776253f9fb68b02f4849eac5bd45
k.kurochkin <kirill.kurochkin@synchro.pro>
2026-03-15 23:58:26 +0300

    upd 15/03/2026

--- data/credentials.py
+++ data/credentials.py
@@ -8,11 +8,11 @@ class Credentials:
     STAGE = os.getenv("STAGE")
 
     if STAGE == "stage1":
-        LOGIN = os.getenv("LOGIN_1")
+        LOGIN = os.getenv("LOGIN_STANDARD")
         PASSWORD = os.getenv("PASSWORD")
     elif STAGE == "stage2":
-        LOGIN = os.getenv("LOGIN_2")
+        LOGIN = os.getenv("LOGIN_LOCKED_OUT")
         PASSWORD = os.getenv("PASSWORD")
     elif STAGE == "stage3":
-        LOGIN = os.getenv("LOGIN_3")
+        LOGIN = os.getenv("LOGIN_PROBLEM")
         PASSWORD = os.getenv("PASSWORD")
